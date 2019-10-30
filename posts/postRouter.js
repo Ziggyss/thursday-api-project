4719e7e73e83e58f0a667f3336bb365a65fb7834
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-30 15:28:50 +0000

    stretch goal - postRouter.gets

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,46 +1,49 @@
-const express = 'express';
-const Posts = require('../posts/postDb');
+const express = "express";
+const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
-
+router.get("/", (req, res) => {
+  Posts.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", validatePostId, (req, res) => {
+  const post = req.post;
+  res.status(200).json(post);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-    const { id } = req.params;
-    Posts.getById(id)
-      .then(post => {
-        if (post) {
-          req.post = post;
-          next();
-        } else {
-          res.status(400).json({
-            message: "Invalid post id"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: err.message
+  const { id } = req.params;
+  Posts.getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(400).json({
+          message: "Invalid post id"
         });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
       });
-
-};
+    });
+}
 
 module.exports = router;
