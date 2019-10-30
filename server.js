2d0c5d80b7bdad1e38f3a4412bbaf9887607e25a
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-30 12:37:31 +0000

    add and test logger function

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log({
+    method: req.method,
+    url: req.url,
+    timestamp: Date.now(),
+  });
+  next();
 };
+
 
 module.exports = server;
