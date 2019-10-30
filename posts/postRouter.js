b96aa980914c163b0a5803203a5bc678279db010
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-30 15:24:39 +0000

    postRouter middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 
+
 });
 
 router.get('/:id', (req, res) => {
@@ -22,6 +23,23 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const { id } = req.params;
+    Posts.getById(id)
+      .then(post => {
+        if (post) {
+          req.post = post;
+          next();
+        } else {
+          res.status(400).json({
+            message: "Invalid post id"
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err.message
+        });
+      });
 
 };
 
